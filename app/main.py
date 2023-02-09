ba0a2b2ddad62ef5c89bf0edb58cfdf3b21747aa
lelandfowler <lelandplusplus@gmail.com>
2023-02-09 12:10:48 -0800

    feat: set up debugging in the main file by launching with uvicorn on call of main

--- app/main.py
+++ app/main.py
@@ -1,5 +1,4 @@
-from typing import Union
-
+import uvicorn
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -8,3 +7,7 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host='127.0.0.1', port=8000)
