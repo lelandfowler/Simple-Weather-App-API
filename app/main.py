8bf0fa3776a4cb277ec4369486ad065b37664372
lelandfowler <lelandplusplus@gmail.com>
2023-02-09 14:31:49 -0800

    add a function clean_weather_data to hold logic for extracting data from the weather api data, in addition add logic to pass through errors on the weather api request

--- app/main.py
+++ app/main.py
@@ -1,10 +1,16 @@
+import json
+from typing import Dict
 from decouple import config
 from datetime import datetime, date, timedelta
 import requests
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Query, Path, HTTPException
 
 app = FastAPI()
+
+
+def clean_weather_data(json_data: Dict):
+    return json_data
 
 
 @app.get("/")
@@ -13,14 +19,30 @@ def read_root():
 
 
 @app.get("/{city_name}")
-def get_weather_data(city_name: str, request_date: date):
-    # Take the city name and date and return the forcast for that day
-    # If the date is > 3 days then return an error from the current date then
+def get_weather_data(
+        city_name: str,
+        request_date: date = Query(
+            None,
+            description="The date that user would like to see data for.",
+        )
+):
+    """
+    :param city_name:
+    :param request_date: Constrained to be within 1-3 days of the current date
+    :return: a dictionary with datetime as the key and the value as a dictionary of parameters and values
+    {
+        date_time: {
+            temperature: float
+            humidity: int (0,100)
+        }
+    }
+    """
     current_date = date.today()
     date_delta = request_date - current_date
     forcast_days = date_delta.days
     units = "Imperial"
     API_KEY = config('API_KEY')
+
     endpoint = f"http://api.openweathermap.org/data/2.5/forecast?q=" \
                f"{city_name}&" \
                f"appid={API_KEY}&" \
@@ -28,9 +50,12 @@ def get_weather_data(city_name: str, request_date: date):
                f"cnt={forcast_days}"
 
     request = requests.get(url=endpoint)
-    j_request = request.json()
-    status_code = request.status_code
-    return {f"status code: {status_code}": j_request}
+    if request.status_code != 200:
+        # Pass exceptions in the weather api call through
+        raise HTTPException(status_code=request.status_code, detail=json.loads(request.text))
+    data = request.json()
+    result = clean_weather_data(data)
+    return result
 
 
 if __name__ == "__main__":
