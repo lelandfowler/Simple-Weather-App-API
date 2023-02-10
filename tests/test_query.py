6d3b1c15a59563c743749b11906133594edf55bf
lelandfowler <lelandplusplus@gmail.com>
2023-02-09 22:32:24 -0800

    test: create query test for the get_weather_data endpoint

--- tests/test_query.py
+++ tests/test_query.py
@@ -1,3 +1,4 @@
+from datetime import date, timedelta
 from fastapi.testclient import TestClient
 from app.main import app
 
@@ -8,3 +9,12 @@ def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {'Hello': 'World'}
+
+
+def test_get_weather_data():
+    params = {
+        "request_date": str(date.today() + timedelta(1))
+    }
+    city_name = 'Los Angeles'
+    response = client.get(f"/{city_name}", params=params)
+    assert response.status_code == 200
