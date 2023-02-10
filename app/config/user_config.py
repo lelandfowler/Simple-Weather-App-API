7effc132bb0b7a6fa50bb2108df059d85a9eb2a8
lelandfowler <lelandplusplus@gmail.com>
2023-02-10 12:03:44 -0800

    bug: correct user_config.py imports

--- app/config/user_config.py
+++ app/config/user_config.py
@@ -1,4 +1,4 @@
-from app.services import User
+from app.models.models import User
 
 users = {'user_1': User(favorites=['Austin', 'Chicago', 'New York']),
          'user_2': User(favorites=['Tulsa', 'Dublin', 'Paris']),
