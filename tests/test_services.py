17a6af2e672986e524773fbf5f7229756ef08b9f
lelandfowler <lelandplusplus@gmail.com>
2023-02-09 20:29:50 -0800

    test: update test_time_point_calculation to account for new logic and confirm that tests are passed

--- tests/test_services.py
+++ tests/test_services.py
@@ -1,20 +1,18 @@
 from datetime import timedelta, date
-
 import pytest
 
 from app.services import time_point_calculation
 
 
 @pytest.mark.parametrize("days_from_today, expected_result", [
-    (1, [0, 6]),
-    (2, [7, 15]),
-    (3, [16, 23]),
+    (1, 8),
+    (2, 16),
+    (3, 24),
 ])
 def test_time_point_calculation(days_from_today, expected_result):
     test_date = date.today() + timedelta(days_from_today)
-    first_time_point_index, last_time_point_index = time_point_calculation(test_date)
-    assert first_time_point_index == expected_result[0]
-    assert last_time_point_index == expected_result[1]
+    total_count = time_point_calculation(test_date)
+    assert total_count == expected_result
 
 
 def test_call_weather_api():
