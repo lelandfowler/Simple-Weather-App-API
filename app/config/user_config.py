3a44cf9a3802aeb6918fafe59be917150f12a7f2
lelandfowler <lelandplusplus@gmail.com>
2023-02-13 01:57:11 -0800

    style: clean up unused imports and comments in preparations for merge

--- app/config/user_config.py
+++ app/config/user_config.py
@@ -1,10 +1,3 @@
-# from app.models.user_models import User
-
-# users = {'user_1': User(favorites=['Austin', 'Chicago', 'New York']),
-#          'user_2': User(favorites=['Tulsa', 'Dublin', 'Paris']),
-#          'user_3': User(favorites=['Honolulu', 'Tokyo', 'London'])}
-
-# Initialize in memory user store with some users that have favorites
 user_dict = {
     'user_1': ['Austin', 'Chicago', 'New York'],
     'user_2': ['Tulsa', 'Dublin', 'Paris'],
