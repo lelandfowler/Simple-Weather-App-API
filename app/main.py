6aaa60ee0302361489d79248d2163531899c82b2
lelandfowler <lelandplusplus@gmail.com>
2023-02-13 09:14:49 -0800

    feat: make uvicorn auto-reload changes to the code for debug purposes

--- app/main.py
+++ app/main.py
@@ -100,4 +100,4 @@ app.include_router(graphql_app, prefix="/graphql")
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host='127.0.0.1', port=8000)
+    uvicorn.run(app, host='127.0.0.1', port=8000, reload=True)
